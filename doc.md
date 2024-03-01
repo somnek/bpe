d5e2a6e4b41d61e54f2ff8d083192ab0b5bf55c0
somnek <mumscarpetiswet@gmail.com>
2024-03-01 09:19:18 +0800

    working example

--- doc.md
+++ doc.md
@@ -1,20 +1,44 @@
 #### s.encode('utf-8') in Go:
-- by default, go already encode to utf-8
+
 - just use `[]byte(someString)`
+- `[]byte()` longer than `[]rune()`, this makes sense because thats the reason by **bytes** are longer than **runes** especially in unicode, unicode joins multiple bytes to form a single character (**bytes** are normalize in 1...256), while **runes** can go beyond just 256 (1 each)
 - 안녕하세요 = [236 149 136 235 133 149 237 149 152 236 132 184 236 154 148]
 - in python, you need to list(map(int)) to it, shorter in go
+- based on the video:
 
+  1. find length of string
+
+  - py: `len(text)`
+  - go: `len([]rune(text)`
+
+  2. find length of bytes
+
+  - py: `len(text.encode('utf-8'))`
+  - go: `len([]byte(text)`
+
+  3. example 1:
+
+  - py: `[ord(x)for x in "안녕하세요 👋 (hello in Korean!)"]`
+  - go: `[]rune()`
+
+  4. example 2:
+
+  - py: `list("안녕하세요 👋 (hello in Korean!)".encode("utf-8"))`
+  - go: `[]byte()`
 
 #### ord() & chr() in go
+
 - **ord()**: rune('a') -> 97
 - **chr()**: string(byte(97)) -> 'a'
 
-
 #### token length bytes vs string
-* if string unicode, its always gonna be bigger than normal string, because they *expands*
-* e.g.:
+
+- if string unicode, its always gonna be bigger than normal string, because they _expands_
+- e.g.:
+
 ```go
-// HELLO is a var of complex string
-inString := HELLO // len 533
-inByte := []byte(HELLO) // len: 616
+// TEXT is a var of complex string
+inString := TEXT // len 533
+inByte := []byte(TEXT) // len: 616
+inByte := []rune(TEXT) // len: 533
 ```
--- main.go
+++ main.go
@@ -1,20 +1,93 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	// 'ids' in karpathy's video
-	// normalize to 0 - 256 (convert bytes to runes)
-	// convert string to runes right away won't works because the values goes beyond 256
-	bytes := []byte(WIKI)
-	runes := bytesToRunes(bytes)
-	stats := getStats(runes)
-	fmt.Println(stats)
+	merges, encoded := encode(WHOLE_TEXT)
+	encodedStr := string(runesToBytes(encoded))
+	fmt.Printf("encoded: \n%s\n    length: %d\n", encodedStr, len(encodedStr))
 
+	decoded := decode(merges, encoded)
+	decodedStr := string(runesToBytes(decoded))
+	fmt.Printf("decoded: \n%v\n", decodedStr)
+
+	fmt.Printf("accurately decoded: %v\n    length: %d\n", decodedStr == WHOLE_TEXT, len(decodedStr))
 }
 
 type Pair struct {
 	a, b rune
+}
+
+type Row struct {
+	pair  Pair
+	alien rune
+}
+
+// -----------------------------------------------------------------------------
+//
+//	COMPRESS/ ENCODE
+//
+// -----------------------------------------------------------------------------
+func encode(text string) ([]Row, []rune) {
+	// 'ids' in karpathy's video
+	// normalize to 0 - 256 (bytes, which has longer length)
+	// convert string to runes right away won't works because the rune values > 256 (we want to stay i 0-256)
+	// thats why we convert to bytes first then runes, which keep same length as original
+	bytes := []byte(text)
+	runes := bytesToRunes(bytes)
+	var alien int32
+	iteration := VOCAB_SIZE - 256
+	// use for decoding (FIFO)
+	var merges []Row
+
+	for j := 0; j < iteration; j++ {
+		stats := getStats(runes)
+		maxPair, maxCount := findMaxPair(stats)
+		if maxCount <= 1 {
+			break
+		}
+		alien = 256 + rune(j)
+		runes = merge(runes, maxPair, alien)
+		// NOTE: to convert back, turn into []rune first then turn the rune to byte
+		// like so: string(runesToBytes(runes))
+		merges = append([]Row{{maxPair, alien}}, merges...) // newest on top
+	}
+	return merges, runes
+}
+
+// -----------------------------------------------------------------------------
+//
+//	DECOMPRESS/ DECODE
+//
+// -----------------------------------------------------------------------------
+func decode(merges []Row, runes []rune) []rune {
+	for _, e := range merges {
+		pairJoined := string(e.pair.a) + string(e.pair.b)
+		runes = []rune(strings.ReplaceAll(string(runes), string(e.alien), pairJoined))
+	}
+	return runes
+}
+
+func merge(runes []rune, pair Pair, alien rune) []rune {
+	pairJoined := string(pair.a) + string(pair.b)
+	s := string(runes)
+	s = strings.ReplaceAll(s, pairJoined, string(alien))
+	return []rune(s)
+}
+
+func findMaxPair(stats map[Pair]int) (Pair, int) {
+	var maxCount int
+	var maxPair Pair
+	for pair, count := range stats {
+		if count > maxCount {
+			maxPair = pair
+			maxCount = count
+		}
+	}
+	return maxPair, maxCount
 }
 
 func getStats(runes []rune) map[Pair]int {
